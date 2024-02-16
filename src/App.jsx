1964d0978c723b5eb270c1e4ee7e93276199546f
JordanGidman <jordanleegidman@hotmail.com>
2024-02-16 16:38:56 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+//This is a more imperative way of routing with react-router, what i am doing here is necessary to tell react-router that we are dealing with data fetching and loading and enables it
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
