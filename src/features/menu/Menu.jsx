6cfe905a2c602399347c90bb31292170931d8356
JordanGidman <jordanleegidman@hotmail.com>
2024-02-17 16:25:59 +0000

    Data loading with react-router
    Today i got into a topic i feel is going to be incredibly useful for my freelance site, which is loading images at the time of render using a loader from react-router, shifting the responsibility for the data loading to react-router, which should improve performance.

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  console.log(menu);
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+//loading menu data from an api using loader which is from react-router and makes it so that the fetch and render happen at the same time
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
