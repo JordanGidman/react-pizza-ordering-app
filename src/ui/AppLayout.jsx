456da9c8db90e63ecc4ecbbf7041f058ca53cd37
JordanGidman <jordanleegidman@hotmail.com>
2024-02-21 16:01:42 +0000

    Tailwind
    I have begun learning tailwind CSS as i have encountered a few job posts that are looking for experience with it. So i spent my time refactoring and styling today along side a ton of reading up and learning about tailwind. Its in fact quite easy with the help of the docs to memorize the changed property names it seems mostly if you know the css properties you can find anything you need to know so i feel i will be mostly just getting used to using it and trying to memorize the necessary class names. Tailwind also seems to favour mobile first development and so i am adapting my approach to suit that. So far i feel it could be an incredibly useful tool in speeding up my workflow despite the face it does clutter the JSX / HTML classes.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,27 +1,28 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router-dom'
+import CartOverview from '../features/cart/CartOverview'
+import Header from './Header'
+import Loader from './Loader'
 
 function AppLayout() {
-  //This will return an object, one of the properties of this is the state, which we can use to tell if router is in the middle of loading a page to then display a loading component
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+    //This will return an object, one of the properties of this is the state, which we can use to tell if router is in the middle of loading a page to then display a loading component
+    const navigation = useNavigation()
+    const isLoading = navigation.state === 'loading'
 
-  return (
-    <div className="layout">
-      {isLoading && <Loader />}
+    return (
+        <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
+            {isLoading && <Loader />}
 
-      <Header />
+            <Header />
+            <div className="overflow-scroll">
+                <main className="mx-auto max-w-3xl ">
+                    {/* This component will render whatever our current child route is, which is why they are all structured as children of the AppLayout route */}
+                    <Outlet />
+                </main>
+            </div>
 
-      <main>
-        {/* This component will render whatever our current child route is, which is why they are all structured as children of the AppLayout route */}
-        <Outlet />
-      </main>
-
-      <CartOverview />
-    </div>
-  );
+            <CartOverview />
+        </div>
+    )
 }
 
-export default AppLayout;
+export default AppLayout
