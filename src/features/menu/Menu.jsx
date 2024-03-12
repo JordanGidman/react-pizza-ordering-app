07d2d49481dde8f5280554b78777d77da7390422
Jordan Gidman <jordanleegidman@hotmail.com>
2024-03-12 14:45:22 +0000

    Styling Components with tailwind
    I have been a bit neglectful of my commits over the last few days and so they are all bundled into this one, i have used what i have learned so far to style most of the remaining components to my liking using all the things ive learned along the way.
    
    I have also had a play around with tailwinds configuration options and can see the power and freedom to customize tailwind that comes along with it. Im very excited to continue using it for now. As a point of personal preference however i am not a huge fan of writing the styles into the class names. I do much prefer my CSS to be abstracted to a new file so i will continue that route for now. I felt tailwind would be useful in landing a job that may operate with a mobile first development strategy.

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,24 +1,24 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom'
+import { getMenu } from '../../services/apiRestaurant'
+import MenuItem from './MenuItem'
 
 function Menu() {
-  const menu = useLoaderData();
-  console.log(menu);
+    const menu = useLoaderData()
+    console.log(menu)
 
-  return (
-    <ul>
-      {menu.map((pizza) => (
-        <MenuItem pizza={pizza} key={pizza.id} />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="divide-y divide-stone-200 px-2">
+            {menu.map((pizza) => (
+                <MenuItem pizza={pizza} key={pizza.id} />
+            ))}
+        </ul>
+    )
 }
 
 //loading menu data from an api using loader which is from react-router and makes it so that the fetch and render happen at the same time by passing this loader to our browser router
 export async function loader() {
-  const menu = await getMenu();
-  return menu;
+    const menu = await getMenu()
+    return menu
 }
 
-export default Menu;
+export default Menu
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            // This colors object needs to go in the extends so that we have access to all tailwinds original colors as well as whichever we choose to add
+            colors: {
+                pizza: '#000',
+            },
+            height: {
+                screen: '100dvh',
+            },
+        },
+        fontFamily: {
+            sans: 'Roboto Mono, monospace',
+        },
+    },
+    plugins: [],
+}
